83fd7c8cf73932d7bd4b11df56f45048ebb5add0
Fernando Cuervo <63762598+Fcuervo21@users.noreply.github.com>
2021-10-17 13:25:28 -0500

    Final Rege Version

--- Rege.py
+++ Rege.py
@@ -1,5 +1,4 @@
 import re
-
 NUMEROS = []
 
 def printSavedValues():
@@ -25,27 +24,34 @@ def checkOperator(lexical_string):
             '%': 'MODULE'
         }
         printSavedValues()
-        print(f'TOKEN TEXT: {m.group(0)}') #El simbolo
-        print(f'TOKEN LEXICAL CATEGORY: {switcher[m.group(0)]}') # El tipo
+        print(f'TOKEN TEXT: {m.group(0)}') #Symbol
+        print(f'TOKEN LEXICAL CATEGORY: {switcher[m.group(0)]}') # Kind of operator
 
 def checkNumbers(lexical_string):
-    # Confirma si es un numero
+    # Check if it is a number
     m = re.match(r'[0-9]', lexical_string)
     
     if m:
         NUMEROS.append(m.group(0))
 
 def checkLetters(lexical_string):
-    # Confirma si es letra
+    # Check if its letter
     m = re.match(r'[a-zA-Z]', lexical_string)
     
     if m:
         print(f'TOKEN TEXT: {m.group(0)}')
         print(f'TOKEN LEXICAL CATEGORY: OPERAND')
 
+def checkBlank(lexical_string):
+    #Check if blank
+    if lexical_string == ' ':
+        printSavedValues()
+
+
 lexical_string = input(str("Input your string:"))
 for letter in lexical_string:
     checkOperator(letter)
+    checkBlank(letter)
     checkNumbers(letter)
     checkLetters(letter)
 printSavedValues()
